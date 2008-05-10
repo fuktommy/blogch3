5a4176863cf0c6c2a7d9289c823a287db9ada3fb
Satoshi Fukutomi <info@fuktommy.com>
2008-05-10 15:35:09 +0000

    Googleモバイルアドセンスのバージョンアップ。
    git-svn-id: http://repos.fuktommy.com/svn/main/blogch3/trunk@38 5ff462cd-d70c-4577-abd0-42c2245143fb

--- plugins/function.mobileAdsense.php
+++ plugins/function.mobileAdsense.php
@@ -4,24 +4,8 @@ function smarty_function_mobileAdsense($params, $smarty)
     $ua = $smarty->get_template_vars('ua');
     if ($ua['ads'] == 'google') {
         ini_set('default_socket_timeout', 5);
-        $GLOBALS['google']['ad_type']   = 'text';
-        $GLOBALS['google']['channel']   = '';
-        $GLOBALS['google']['client']    = 'pub-0908882948816599';
-        $GLOBALS['google']['format']    = 'mobile_single';
-        $GLOBALS['google']['https']     = @$_SERVER['HTTPS'];
-        $GLOBALS['google']['host']      = @$_SERVER['HTTP_HOST'];
-        $GLOBALS['google']['ip']        = @$_SERVER['REMOTE_ADDR'];
-        $GLOBALS['google']['markup']    = 'xhtml';
-        $GLOBALS['google']['oe']        = 'utf8';
-        $GLOBALS['google']['output']    = 'xhtml';
-        $GLOBALS['google']['ref']       = @$_SERVER['HTTP_REFERER'];
-        $GLOBALS['google']['url']       = @$_SERVER['HTTP_HOST'] . @$_SERVER['REQUEST_URI'];
-        $GLOBALS['google']['useragent'] = @$_SERVER['HTTP_USER_AGENT'];
-        $script = file_get_contents('http://pagead2.googlesyndication.com/pagead/show_ads.php');
-        if ($script) {
-            $script = preg_replace('/^<\?php|\?>\s*$/', '', $script);
-            eval($script);
-        }
+        include('googleMobileAdsense.php');
+
     } elseif ($ua['ads'] == 'amazon') {
         // $files にディレクトリ内のファイル一覧を積む
         $conf = blogconfig();
