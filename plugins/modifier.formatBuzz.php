3e3837cfcd2d95a7f70d5fd8aa7e99c87b039739
Satoshi Fukutomi <info@fuktommy.com>
2010-12-06 05:05:16 +0000

    モバイル用の出力を見てなかった。
    git-svn-id: http://repos.fuktommy.com/svn/main/blogch3/trunk@375 5ff462cd-d70c-4577-abd0-42c2245143fb

--- plugins/modifier.formatBuzz.php
+++ plugins/modifier.formatBuzz.php
@@ -5,10 +5,8 @@ function smarty_modifier_formatBuzz($value)
         '<wbr>' => '',
         '<b>' => '',
         '</b>' => '',
-        '</blockquote>' => '',
     ));
-    $value = preg_replace('|<p>Reshared post from \s+</p>|', '', $value);
-    $value = preg_replace('|<blockquote .*?>|', '', $value);
+    $value = preg_replace('|<p>Reshared post from\s*</p>\s*<blockquote .*?>(.*)</blockquote>\s*$|s', '$1', $value);
     $value = preg_replace('/^(\s|<br>)+/', '', $value);
     $value = preg_replace('|(<a href="http://blogsearch.google.co.jp/blogsearch[?]hl=ja&amp;[^"\' ]*)&amp;ie=utf-8|', '$1', $value);
     return $value;
--- templates/mobile_buzz.tpl
+++ templates/mobile_buzz.tpl
@@ -4,7 +4,7 @@
 {mobileAdsense}
 <ul>
 {foreach from=$buzz->entry item="entry"}
-    <li><a href="http://www.google.co.jp/gwt/x?source=wax&amp;ie=UTF-8&amp;oe=UTF-8&amp;u={$entry->link.href|replace:"/buzz/104787602969620799839/":"/buzz/fuktommy/"|escape:"url"}">{$entry->summary|mb_substr:0:30:"utf8"}</a></li>
+    <li><a href="http://www.google.co.jp/gwt/x?source=wax&amp;ie=UTF-8&amp;oe=UTF-8&amp;u={$entry->link.href|replace:"/buzz/104787602969620799839/":"/buzz/fuktommy/"|escape:"url"}">{$entry->content|formatBuzz|strip_tags|mb_substr:0:30:"utf8"}</a></li>
 {/foreach}
 </ul>
 {include file="mobile_footer.tpl"}
