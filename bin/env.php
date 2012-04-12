c2ae41da3853d93f3c5d5163401f05a09c311fda
Satoshi Fukutomi <info@fuktommy.com>
2012-04-12 11:11:17 +0900

    join buzz directory to lib top level

--- bin/env.php
+++ bin/env.php
@@ -2,5 +2,4 @@
 ini_set('include_path',
         '/usr/share/php:/srv/lib/php'
         . ':/usr/share/php/smarty/libs'
-        . ':/srv/lib/php/blog.fuktommy.com'
-        . ':/srv/lib/php/blog.fuktommy.com/buzz');
+        . ':/srv/lib/php/blog.fuktommy.com');
