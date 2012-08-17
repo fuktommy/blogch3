13d09a576063bb80cf985a873aa1f3938826d9b3
Satoshi Fukutomi <info@fuktommy.com>
2012-08-17 09:28:56 +0900

    do not display amazon image to avoid adsense of linked page

--- plugins/function.buzzImage.php
+++ plugins/function.buzzImage.php
@@ -1,7 +1,7 @@
 <?php
 /* バズに含まれる画像情報を読み込む。
  *
- * Copyright (c) 2010 Satoshi Fukutomi <info@fuktommy.com>.
+ * Copyright (c) 2010,2012 Satoshi Fukutomi <info@fuktommy.com>.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -50,9 +50,7 @@ function smarty_function_buzzImage($params, $smarty)
             foreach ($attach->link as $link) {
                 if ($link['rel'] == 'enclosure') {
                     $host = parse_url($link['href'], PHP_URL_HOST);
-                    if (preg_match('/[.]images-amazon[.]com$/', $host)) {
-                        $img['is_amazon'] = true;
-                    } elseif (preg_match('/[.]ggpht[.]com$/', $host)) {
+                    if (preg_match('/[.]ggpht[.]com$/', $host)) {
                         $img['is_buzz'] = true;
                     } elseif (preg_match('/[.]googleusercontent[.]com$/', $host)
                               && (! preg_match('/proxy/', $link['href']))) {
